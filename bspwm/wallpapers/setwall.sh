5ba40bbdb6baa3fc2498b4814a04af9e5b61953d
mod3sty <marco.yang.ca@gmail.com>
2022-11-08 22:10:47 -0800

    removed bspwm polybar on laptop

--- bspwm/wallpapers/setwall.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-feh --randomize --bg-fill $HOME/.config/bspwm/wallpapers/$1 
--- polybar/scripts/choosemusic.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ $(pgrep -c firefox) -eq 0 ]]
-then
-	echo "ﱘ  No Music Playing"
-else
-	/home/mod3sty/.config/polybar/scripts/browsermediacontrol
-fi
