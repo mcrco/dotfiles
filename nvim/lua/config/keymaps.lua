b971063f0efbdb6004b459a554d990a6f056fb69
Marco Yang <marco.yang.ca@gmail.com>
2024-11-16 16:43:32 -0800

    change keymaps following updaet

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,13 +1,10 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local map = vim.keymap.set
 
-local map = LazyVim.safe_keymap_set
-
-vim.keymap.del("n", "<leader>bb")
 map("n", "<leader>bb", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
 map("n", "<leader>bn", "<cmd>bnext<cr>", { desc = "Next Buffer" })
-map("n", "<leader>c", LazyVim.ui.bufremove, { desc = "Delete Buffer" })
+map("n", "<leader>c", function()
+    Snacks.bufdelete()
+end, { desc = "Delete Buffer" })
 
 local Util = require("lazyvim.util")
 vim.keymap.set("n", "<C-/>", function()
