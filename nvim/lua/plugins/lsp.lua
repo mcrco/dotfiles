fbba6c98cb6a7437755c87c88ea5771484140df8
Marco Yang <marco.yang.ca@gmail.com>
2025-11-22 19:07:07 -0800

    use ty

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,22 +1,32 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = function(_, opts)
-      opts.servers = {
-        lua_ls = {
-          settings = {
-            Lua = {
-              format = {
-                enable = true,
-                defaultConfig = {
-                  indent_style = "space",
-                  indent_size = "4",
+    {
+        "neovim/nvim-lspconfig",
+        opts = function(_, opts)
+            require("lspconfig.configs").ty = {
+                default_config = {
+                    cmd = { "ty", "server" },
+                    filetypes = { "python" },
+                    root_dir = require("lspconfig.util").root_pattern("pyproject.toml", ".git"),
+                    single_file_support = true,
+                    settings = {},
                 },
-              },
-            },
-          },
-        },
-      }
-    end,
-  },
+            }
+            opts.servers = {
+                lua_ls = {
+                    settings = {
+                        Lua = {
+                            format = {
+                                enable = true,
+                                defaultConfig = {
+                                    indent_style = "space",
+                                    indent_size = "4",
+                                },
+                            },
+                        },
+                    },
+                },
+                ty = {},
+            }
+        end,
+    },
 }
